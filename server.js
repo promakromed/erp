c972a566ccd02041b7fa56bc0f940b4d592ac092
promakromed <haci@promakromed.com>
2025-04-18 16:51:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Load environment variables
 dotenv.config();
