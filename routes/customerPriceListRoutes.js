46e9b0a017fb2a690d459b29ae2ad62125d49e57
promakromed <haci@promakromed.com>
2025-05-08 14:56:13 +0300

    Update customerPriceListRoutes.js

--- routes/customerPriceListRoutes.js
+++ routes/customerPriceListRoutes.js
@@ -1,29 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-// Middleware
 const { protect, admin } = require("../middleware/authMiddleware");
-
-// Controller functions
 const {
     getCustomerPriceLists,
     getCustomerPriceListById,
     createCustomerPriceList,
     updateCustomerPriceList,
     deleteCustomerPriceList
-} = require("../controllers/priceListController");
+} = require("../controllers/customerPriceListController");
 
-// @desc    GET all price lists or CREATE new one
-// @route   GET /api/price-lists
-// @route   POST /api/price-lists
+// @desc    GET all price lists or CREATE new
 router.route("/")
     .get(protect, getCustomerPriceLists)
     .post(protect, admin, createCustomerPriceList);
 
-// @desc    GET, UPDATE, DELETE single price list
-// @route   GET /api/price-lists/:id
-// @route   PUT /api/price-lists/:id
-// @route   DELETE /api/price-lists/:id
+// @desc    GET / UPDATE / DELETE single price list
 router.route("/:id")
     .get(protect, getCustomerPriceListById)
     .put(protect, admin, updateCustomerPriceList)
