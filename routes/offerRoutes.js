05168427980f25e54556750376f8b68b8671eefb
promakromed <haci@promakromed.com>
2025-05-09 10:57:44 +0300

    Update offerRoutes.js

--- routes/offerRoutes.js
+++ routes/offerRoutes.js
@@ -15,39 +15,32 @@ const {
     generateOfferCsv
 } = require("../controllers/offerController");
 
-// @desc    Get all offers
+// @desc    Fetch all offers
 // @route   GET /api/offers
-// @access  Private
 router.route("/").get(protect, getOffers);
 
-// @desc    Create a new offer
-// @route   POST /api/offers
-// @access  Private
-router.route("/").post(protect, createOffer);
-
-// @desc    Get single offer by ID
+// @desc    Fetch single offer by ID
 // @route   GET /api/offers/:id
-// @access  Private
 router.route("/:id").get(protect, getOfferById);
 
-// @desc    Update an offer
+// @desc    Create new offer
+// @route   POST /api/offers
+router.route("/").post(protect, admin, createOffer);
+
+// @desc    Update existing offer
 // @route   PUT /api/offers/:id
-// @access  Private
-router.route("/:id").put(protect, updateOffer);
+router.route("/:id").put(protect, admin, updateOffer);
 
-// @desc    Delete an offer (only if Draft)
+// @desc    Delete offer (only if Draft)
 // @route   DELETE /api/offers/:id
-// @access  Private
-router.route("/:id").delete(protect, deleteOffer);
+router.route("/:id").delete(protect, admin, deleteOffer);
 
-// @desc    Generate PDF of offer
+// @desc    Generate PDF of an offer
 // @route   GET /api/offers/:id/pdf
-// @access  Private
 router.route("/:id/pdf").get(protect, generateOfferPdf);
 
-// @desc    Generate CSV of offer
+// @desc    Generate CSV of an offer
 // @route   GET /api/offers/:id/csv
-// @access  Private
 router.route("/:id/csv").get(protect, generateOfferCsv);
 
 module.exports = router;
